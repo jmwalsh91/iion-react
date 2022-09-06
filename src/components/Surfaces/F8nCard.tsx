04a4935ae7eded2e801626527ac4c62feb040aac
jmwalsh91 <jordanmwalsh@gmail.com>
2022-09-05 19:45:14 -0600

    task: collection query, F8nCard renders correct information correctly

--- src/components/Surfaces/F8nCard.tsx
+++ src/components/Surfaces/F8nCard.tsx
@@ -1,44 +1,25 @@
 import { useMarketInfo } from '@f8n/foundationkit-hooks'
-import { Paper, Title, Text } from '@mantine/core'
+import { Paper, Title, Text, Skeleton } from '@mantine/core'
 import { ListItem } from '@mantine/core/lib/List/ListItem/ListItem'
+import { useQuery, useQueryClient, UseQueryResult } from '@tanstack/react-query'
 import React, { Suspense, useEffect } from 'react'
 import { useContract, useContractRead, useProvider } from 'wagmi'
-import {
-  CollectionContractResponse,
-  NFTDetails,
-  TokenMetadata,
-} from '~/types/components'
+import { NFTDetails } from '~/types/components'
+import { getCollection } from '~/utils/services/fetchers'
 import ABI from '../../utils/services/ABI.json'
 type Props = {
   contractAddress: string
 }
 
-function reshapeFromTatum(item: CollectionContractResponse): NFTDetails {
-  return {
-    tokenId: item.tokenId,
-    name: item.metadata.metadata.name,
-    description: item.metadata.metadata.description,
-    image: item.metadata.metadata.image,
-  }
-}
 function F8nCard({ contractAddress }: Props) {
   const provider = useProvider()
-
-  const items: any = fetch(
-    'https://api-us-west1.tatum.io/v3/nft/collection/ETH/0x0d2166B7bF87Dab6C19D55197c774983BcC00704?pageSize=50'
-  )
-    .then((res) => res.json())
-    .then(async (data) => {
-      const data2 = await data.map((item: any) => {
-        console.log(item)
-        const result = item.metadata ? reshapeFromTatum(item) : null
-        return result
-      })
-      return data2
-    })
-    .then((data2) => console.log(data2))
-    .catch((err) => console.log(err))
-
+  const queryClient = useQueryClient()
+  const { isLoading, error, data }: UseQueryResult<NFTDetails[] | null> =
+    useQuery(
+      ['collection'],
+      //TODO: fix typing
+      () => getCollection('0x0d2166B7bF87Dab6C19D55197c774983BcC00704') as any
+    )
   /* const con = useContractRead({
     addressOrName: contractAddress,
     contractInterface: ABI,
@@ -46,15 +27,21 @@ function F8nCard({ contractAddress }: Props) {
     args: 'balanceOf',
   }) */
 
-  const { data } = useMarketInfo({
+  /*   const { data } = useMarketInfo({
     provider,
     contractAddress: '0x0d2166B7bF87Dab6C19D55197c774983BcC00704',
     tokenId: 1,
-  })
+  }) */
+  if (isLoading) {
+    return <Skeleton height="3rem" />
+  }
+  if (error) {
+    return <div>{error as string}</div>
+  }
 
   return (
     <Suspense>
-      {items?.map((item: NFTDetails) => {
+      {data?.map((item: NFTDetails) => {
         return (
           <Paper key={item.tokenId}>
             <Title>{item.name} </Title>
