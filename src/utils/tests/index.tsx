72ce8e6931ff484e719bf9710c3846c5d50011d1
jmwalsh91 <jordanmwalsh@gmail.com>
2022-09-09 03:04:49 -0600

    fix: customRender function for vitest, provide queryProvider and wagmi context

--- src/utils/tests/index.tsx
+++ src/utils/tests/index.tsx
@@ -1,24 +1,29 @@
-import {
-  RenderOptions,
-  RenderResult,
-  cleanup,
-  render,
-} from '@testing-library/react'
+import { Provider } from '@f8n/foundationkit-hooks'
+import { RenderResult, cleanup, render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { ReactElement } from 'react'
 import { afterEach } from 'vitest'
+import { WagmiConfig } from 'wagmi'
+
+import { QueryClientContext } from '../services/QueryClient'
+import { wagmiClient } from '../services/WagmiClient'
 
 afterEach(() => {
   cleanup()
 })
 
 const customRender = (
-  ui: ReactElement,
-  options: RenderOptions = {}
+  ui: ReactElement
+  /*   options: RenderOptions = {} */
 ): RenderResult =>
   render(ui, {
-    wrapper: ({ children }) => <div> {children} </div>,
-    ...options,
+    wrapper: ({ children }) => (
+      <WagmiConfig client={wagmiClient}>
+        <Provider>
+          <QueryClientContext>{children}</QueryClientContext>
+        </Provider>{' '}
+      </WagmiConfig>
+    ),
   })
 
 export * from '@testing-library/react'
--- src/utils/tests/landing.test.tsx
+++ src/utils/tests/landing.test.tsx
@@ -1,9 +1,10 @@
-import { render } from '@testing-library/react'
 import { screen } from '@testing-library/react'
 import { it } from 'vitest'
 import { describe } from 'vitest'
 
 import Index from '../../routes/Index'
+
+import { render } from './index'
 
 describe('landing page', () => {
   it('should render the landing page', () => {
