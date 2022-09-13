41dc830d9f73c6658e65c7bebf1a029146243cae
jmwalsh91 <jordanmwalsh@gmail.com>
2022-09-12 23:58:10 -0600

    task: collectioncard with mockprops in collection page for styling

--- src/routes/Collection.tsx
+++ src/routes/Collection.tsx
@@ -1,6 +1,8 @@
 import { Container, Paper, Title } from '@mantine/core'
 
 import Section from '~/components/Motion/Section'
+import CollectionCard from '~/components/Surfaces/CollectionCard'
+import { CollectionCardProp } from '~/utils/mocks/FoundationMocks'
 
 function Collection() {
   return (
@@ -16,6 +18,13 @@ function Collection() {
           }}
         >
           <Title>Description</Title>
+          <CollectionCard
+            owner={CollectionCardProp.owner}
+            title={CollectionCardProp.title}
+            description={CollectionCardProp.description}
+            image={CollectionCardProp.image}
+            url={''}
+          />
         </Paper>
       </Section>
     </Container>
